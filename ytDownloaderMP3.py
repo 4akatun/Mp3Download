9e02302b7252e3710b1fe136b9a58ec8612fe30b
akatun <git@4akatun.com>
2022-08-25 21:44:19 +0200

    add: output colors

--- ytDownloaderMP3.py
+++ ytDownloaderMP3.py
@@ -3,24 +3,25 @@
 ## IMPORT LIBRARTY
 from pytube import YouTube
 import os, signal, sys
+from colorama import init, Fore
 
 # CONTRON PROGRAMME MANAGEMENT
 def def_hundler(sig, frame):
-    print("\n\n[!] Exit...\n")
+    print(Fore.RED + "\n\n[!] Exit...\n")
     sys.exit(1)
 
 signal.signal(signal.SIGINT, def_hundler)
 
 ## URL INPUT USER
-yt = YouTube(str(input("Enter the URL of the mp3 you want to download: \n>>\t")))
+yt = YouTube(str(input(Fore.GREEN + "Enter the URL of the mp3 you want to download: \n>>\t")))
 
 ## EXTRACT ONLY AUDIO
 mp3 = yt.streams.filter(only_audio=True).first()
 
 ## directory TO SAVE FILE
-directory = (str(input("Enter the path of save file:\n>>\t")))
+directory = (str(input(Fore.CYAN + "Enter the path of save file:\n>>\t")))
 
-print("Download in your directory path\n>>\t" + directory)
+print(Fore.CYAN + "Download in your directory path\n>>\t" + directory)
 
 ## DOWNLOAD THE FILE
 file_download = mp3.download(output_path=directory)
@@ -31,4 +32,4 @@ new_file = base + '.mp3'
 os.rename(file_download, new_file)
 
 ## RESULT OF SUCCESSFULLY DOWNLOAD AND SAVE FILE
-print("Download Successfully\n>>\t" + yt.title)
+print(Fore.GREEN + "Download Successfully\n>>\t" + yt.title)
