99e4c19c9cad135fef8bbea623177e70ff297ac3
4akatun <111322297+4akatun@users.noreply.github.com>
2022-12-05 19:43:01 +0100

    Update ytDownloaderMP3.PY
    Add search.

--- ytDownloaderMP3.py
+++ ytDownloaderMP3.py
@@ -5,12 +5,23 @@ from pytube import YouTube
 import os, signal, sys
 from colorama import init, Fore
 
-# CONTRON PROGRAMME MANAGEMENT
+## Ctrl + C
 def def_hundler(sig, frame):
     print(Fore.RED + "\n\n[!] Exit...\n")
     sys.exit(1)
-
 signal.signal(signal.SIGINT, def_hundler)
+
+## Search in youtube
+search = (str(input(Fore.GREEN + "\nInsert to search: ")))
+key_word = search
+html = urllib.request.urlopen("https://www.youtube.com/results?search_query=" + key_word)
+video_ids = re.findall(r"watch\?v=(\S{11})", html.read().decode())
+print(Fore.CYAN + "\nhttps://www.youtube.com/watch?v=" + video_ids[0])
+print("https://www.youtube.com/watch?v=" + video_ids[1])
+print("https://www.youtube.com/watch?v=" + video_ids[2])
+print("https://www.youtube.com/watch?v=" + video_ids[3])
+print("https://www.youtube.com/watch?v=" + video_ids[4])
+print("https://www.youtube.com/watch?v=" + video_ids[5])
 
 ## URL INPUT USER
 yt = YouTube(str(input(Fore.GREEN + "Enter the URL of the mp3 you want to download: \n>>\t")))
